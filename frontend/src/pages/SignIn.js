31fe06f84a63feaefeff36d3942aade380936fa9
himani <himani.bhardwaj1@ibm.com>
2025-06-01 01:46:02 +0530

    Adding carbon Design components

--- frontend/src/pages/SignIn.js
+++ frontend/src/pages/SignIn.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import api from '../api';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Button, FormGroup, PasswordInput, Stack, TextInput, Link} from '@carbon/react';
 
 const SignIn = () => {
   const [form, setForm] = useState({ email: '', password: '' });
@@ -18,15 +19,39 @@ const SignIn = () => {
   };
 
   return (
-    <div>
-      <h2>Sign In</h2>
+    <div style={{ padding: '2rem' }}>
       <form onSubmit={handleSubmit}>
-        <input type="email" placeholder="Email" required onChange={e => setForm({ ...form, email: e.target.value })} />
-        <input type="password" placeholder="Password" required onChange={e => setForm({ ...form, password: e.target.value })} />
-        <button type="submit">Sign In</button>
+        <FormGroup
+          legendId="signin-form"
+          legendText="Sign In"
+          style={{ maxWidth: '500px' }}
+        >
+          <Stack gap={7}>
+            <TextInput
+              id="email-input"
+              labelText="Email"
+              type="email"
+              placeholder="you@example.com"
+              required
+              value={form.email}
+              onChange={e => setForm({ ...form, email: e.target.value })}
+            />
+            <PasswordInput
+              id="password-input"
+              labelText="Password"
+              placeholder="Enter your password"
+              required
+              value={form.password}
+              onChange={e => setForm({ ...form, password: e.target.value })}
+            />
+            <Button type="submit">Sign In</Button>
+          </Stack>
+        </FormGroup>
       </form>
-      <p>
-        Don't have an account? <Link to="/signup">Create a new account</Link>
+
+      <p style={{ marginTop: '1rem' }}>
+        Don't have an account ? 
+         <Link href="/signup" >Create a new account</Link>
       </p>
     </div>
   );
--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import api from '../api';
 import { useNavigate, Link } from 'react-router-dom';
+import { Button, FormGroup, PasswordInput, Stack, TextInput} from '@carbon/react';
 
 const SignUp = () => {
   const [form, setForm] = useState({ name: '', email: '', password: '' });
@@ -18,14 +19,42 @@ const SignUp = () => {
   };
 
   return (
-    <div>
-      <h2>Create Account</h2>
+    <div style={{ padding: '2rem' }}>
       <form onSubmit={handleSubmit}>
-        <input type="text" placeholder="Name" required onChange={e => setForm({ ...form, name: e.target.value })} />
-        <input type="email" placeholder="Email" required onChange={e => setForm({ ...form, email: e.target.value })} />
-        <input type="password" placeholder="Password" required onChange={e => setForm({ ...form, password: e.target.value })} />
-        <button type="submit">Sign Up</button>
+        <FormGroup
+          legendId="signup-form"
+          legendText="Sign Up"
+          style={{ maxWidth: '500px' }}
+          >
+            <Stack gap={7}>
+              <TextInput
+                id="name"
+                labelText="Name"
+                type="name"
+                placeholder="username"
+                required
+                onChange={e => setForm({ ...form, name: e.target.value })}
+              />
+              <TextInput
+                id="email-input"
+                labelText="Email"
+                type="email"
+                placeholder="you@example.com"
+                required
+                onChange={e => setForm({ ...form, email: e.target.value })}
+              />
+              <PasswordInput
+                id="password-input"
+                labelText="Password"
+                placeholder="Enter your password"
+                required
+                onChange={e => setForm({ ...form, password: e.target.value })}
+              />
+              <Button type="submit">Sign Up</Button>
+            </Stack>
+        </FormGroup>
       </form>
+
       <button onClick={() => navigate('/')}>Go back to Sign In</button>
     </div>
   );
